c68b431e74d47dec8995b038308848b58c084a86
Restu Edo Setiaji <restuedosetiaji@gmail.com>
2022-05-27 00:20:52 +0700

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var e = document.getElementsByClassName("collapsible");
+
+for (var i = 0; i < e.length; i++) {
+    e[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
